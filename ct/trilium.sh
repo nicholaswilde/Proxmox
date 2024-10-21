007c4cab49ace5745dd5a171a1aacec5b8d3ad01
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2024-10-21 13:55:40 +1000

    Add unsupported to lldap

--- ct/trilium.sh
+++ ct/trilium.sh
@@ -1,4 +1,4 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 # source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
 # # Copyright (c) 2021-2024 tteck
 # # Author: tteck (tteckster)
