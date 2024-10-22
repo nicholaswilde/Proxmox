e3ea08b788ac8db13daa7361abd1b99293aab5c9
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2024-10-22 16:42:09 +1000

    Archivebox: externally managed fix

--- install/archivebox-install.sh
+++ install/archivebox-install.sh
@@ -36,6 +36,7 @@ $STD apt-get install -y \
   python3-ldap \
   python3-msgpack \
   python3-regex
+rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
 msg_ok "Installed Python Dependencies"
 
 msg_info "Setting up Node.js Repository"
