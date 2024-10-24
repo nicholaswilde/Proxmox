d26994c59702100cccf895e54e2fd52c4a1fffac
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2024-10-24 12:37:32 +1000

    Port Bunkerweb to arm64
    somehow missed it

--- install/bunkerweb-install.sh
+++ install/bunkerweb-install.sh
@@ -21,6 +21,7 @@ $STD apt-get install -y gpg
 $STD apt-get install -y apt-transport-https
 $STD apt-get install -y lsb-release 
 $STD apt-get install -y openssh-server
+$STD apt-get install -y wget
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Nginx"
