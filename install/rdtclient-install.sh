d8f7b01640596086678b0af6cb89025d7ea4f292
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2024-11-22 12:07:13 +0000

    Update rdtclient-install.sh
    updated to dotnet 9

--- install/rdtclient-install.sh
+++ install/rdtclient-install.sh
@@ -32,7 +32,7 @@ $STD apt-get install -y libssl3
 $STD apt-get install -y libstdc++6
 $STD apt-get install -y zlib1g
 
-curl -SL -o dotnet.tar.gz https://download.visualstudio.microsoft.com/download/pr/1e449990-2934-47ee-97fb-b78f0e587c98/1c92c33593932f7a86efa5aff18960ed/dotnet-sdk-8.0.204-linux-arm64.tar.gz
+curl -SL -o dotnet.tar.gz https://download.visualstudio.microsoft.com/download/pr/6f79d99b-dc38-4c44-a549-32329419bb9f/a411ec38fb374e3a4676647b236ba021/dotnet-sdk-9.0.100-linux-arm64.tar.gz
 mkdir -p /usr/share/dotnet
 $STD tar -zxf dotnet.tar.gz -C /usr/share/dotnet
 $STD ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
