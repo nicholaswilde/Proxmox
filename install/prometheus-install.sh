35ee9f2b0503fa237917166a9d9d498766eeed06
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2024-12-08 00:11:25 -0800

    fix: remove mv console
    Signed-off-by: nιcнolaѕ wιlde <ncwilde43@gmail.com>

--- install/prometheus-install.sh
+++ install/prometheus-install.sh
@@ -29,7 +29,6 @@ wget -q https://github.com/prometheus/prometheus/releases/download/v${RELEASE}/p
 tar -xf prometheus-${RELEASE}.linux-arm64.tar.gz
 cd prometheus-${RELEASE}.linux-arm64
 mv prometheus promtool /usr/local/bin/
-mv consoles/ console_libraries/ /etc/prometheus/
 mv prometheus.yml /etc/prometheus/prometheus.yml
 echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
 msg_ok "Installed Prometheus"
