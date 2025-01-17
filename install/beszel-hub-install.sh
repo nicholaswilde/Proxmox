fd46bef697776682980d32d7603690dc2fbdf6cd
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2025-01-18 00:04:04 +1000

    add beszel hub script

---
+++ install/beszel-hub-install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+$STD apt-get install -y wget
+$STD apt-get install -y openssh-server
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Beszel Hub"
+cd /opt
+curl -sL https://raw.githubusercontent.com/henrygd/beszel/main/supplemental/scripts/install-hub.sh -o install.sh
+chmod +x install.sh
+$STD ./install.sh
+chmod 666 /opt/runtipi/state/settings.json
+msg_ok "Installed Beszel Hub"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm /opt/install.sh
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
